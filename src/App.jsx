eb3a8cd7b89f0b55aac1063034fa82506dd2ab75
Gugalomi=geo <iva290386@gmail.com>
2024-12-22 00:17:14 +0400

    Make components and show images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import React from "react";
+import data from "./data.json";
 import "./App.css";
 
+const BASE_URL = "https://res.cloudinary.com/dc2c49xov/desserts/";
 
 function App() {
-  return ;
+  return (
+    <div className="container">
+      <h1>Desserts</h1>
+      {data.map((item, index) => (
+        <div key={index}>
+          <img src={`${BASE_URL}${item.images.desktop}`} alt={item.name} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
